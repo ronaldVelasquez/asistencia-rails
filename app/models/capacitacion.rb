47607fa9c8b3d69ce94fc8adb407a7d16f970f4c
Ronald Velasquez <ronaldvg93@gmail.com>
2015-04-28 12:59:41 -0500

    Add cod_sede_operativa and cod_local_sede in ubigeo json

--- app/models/capacitacion.rb
+++ app/models/capacitacion.rb
@@ -4,13 +4,15 @@ class Capacitacion < ActiveRecord::Base
 
   def self.ubigeo(usuario_local)
     usuario = Usuario.find_by(id: usuario_local.id).usuario
-    sede = SedeOperativa.find_by(cod_sede_operativa: usuario_local.cod_sede_operativa).sede_operativa
+    cod_sede_operativa = usuario_local.cod_sede_operativa
+    cod_local_sede = usuario_local.cod_local_sede
+    sede = SedeOperativa.find_by(cod_sede_operativa: cod_sede_operativa).sede_operativa
     local = Local.find_by(cod_sede_operativa: usuario_local.cod_sede_operativa, cod_local_sede: usuario_local.cod_local_sede)
     local_nombre = local.nombreLocal
     local_direccion = local.direccion
     local_estado = local.estado
     local_nro_aula = local.naula
-    json = { usuario: usuario, sede_operativa: sede, local: local_nombre, direccion: local_direccion, aulas: local_nro_aula, estado: local_estado }
+    json = { usuario: usuario, sede_operativa: sede, local: local_nombre, direccion: local_direccion, aulas: local_nro_aula, estado: local_estado, cod_sede_operativa: cod_sede_operativa, cod_local_sede: cod_local_sede }
     json.to_json
   end
 end
